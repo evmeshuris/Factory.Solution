106b4112fa59b94b5a932b0909e12cbf7d9a894c
evmeshuris <101578340+evmeshuris@users.noreply.github.com>
2022-05-27 09:57:08 -0700

    add EngineersController with Create, Edit, Delete and Details functions

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -11,7 +11,8 @@ namespace Factory.Models
 
     public int EngineerId { get; set; }
     public string Name { get; set; }
-    public string Lisence { get; set; }
+    public bool License { get; set; }
+    public string[] LicenseOptions = new[] { "Yes", "No" };
     public virtual ICollection<MachineEngineer> JoinEntities { get; }
   }
 }
